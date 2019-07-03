58427f6a6554822a9658c3326f0ae97b83fb8b6d
GrandMasterTux <grandmaster@tuxlabs.us>
2019-07-02 22:49:28 -0400

    DST Game Server Files
    Updated files by KittyAvenger

---
+++ Master/modoverrides.lua
@@ -0,0 +1,83 @@
+return {
+["workshop-378160973"] = { --Global Position
+    enabled = true,
+    configuration_options =
+    {
+        ["SHOWPLAYERSOPTIONS"] = 1,
+        ["SHOWPLAYERICONS"] = true,
+        ["FIREOPTIONS"] = 3,
+        ["SHOWFIREICONS"] = false,
+        ["SHAREMINIMAPPROGRESS"] = true,
+        ["OVERRIDEMODE"] = false,
+    }
+},
+["workshop-481765222"] = { --Ancient Altar
+    enabled = true,
+    configuration_options =
+    {
+        ["AncientStation"] = "hard",
+        ["ThuleciteFragments"] = "easy",
+    }
+},
+["workshop-375859599"] = { --Health Info
+    enabled = true,
+    configuration_options =
+    {
+        ["show_type"] = 1,
+    }
+},
+--#DST Freezer
+["workshop-346962876"] = { enabled = true },
+--#Superior Wendy
+["workshop-1263779104"] = { enabled = true },
+--#Pickle It
+["workshop-404983266"] = { enabled = true },
+--#Craft Stuff
+["workshop-398115039"] = { enabled = true },
+--#Deluxe Cooking Pot
+["workshop-907007729"] = { enabled = true },
+--#Recipe for Steel Wool
+["workshop-473891973"] = { enabled = true },
+--#Large Chest
+["workshop-396026892"] = { enabled = true },
+--#Fire Resurrection
+["workshop-676297854"] = { enabled = false },
+--#Reliable Webber
+["workshop-1282814181"] = { enabled = true },
+--#Mineable Gems
+["workshop-380423963"] = { enabled = true },
+--#Deluxe Campfires
+["workshop-444235588"] = { enabled = true },
+--#Dynamic Path Lights
+["workshop-874470071"] = { enabled = true },
+--#Miniature Dwellings
+["workshop-710150780"] = { enabled = true },
+--#Phonograph
+["workshop-1727345834"] = { enabled = true },
+--#DST Spider Cage
+["workshop-384396097"] = { enabled = true },
+--#Moving Box
+["workshop-1079538195"] = { enabled = true },
+--#New Walls - Reeds, Hedge, Mud, Living and Bone
+["workshop-806289856"] = { enabled = true },
+--#DST Storm Cellar
+["workshop-382177939"] = { enabled = true },
+--#DST Fish Farm
+["workshop-356435289"] = { enabled = true },
+--#Turfed!
+["workshop-514078314"] = { enabled = true },
+--#Kingdom - Farmer's Hut
+["workshop-1768821433"] = { enabled = true },
+--#Increased Stack Size
+["workshop-374550642"] = { enabled = true },
+--#Infinite Touch Stone
+["workshop-1260611112"] = { enabled = true },
+--#More Inventory
+["workshop-378962221"] = { enabled = true },
+--#Show Me
+["workshop-666155465"] = { enabled = true },
+--#Flower Box DST
+["workshop-512502326"] = { enabled = true }
+
+}
+
---
+++ dedicated_server_mods_setup.lua
@@ -0,0 +1,98 @@
+--There are two functions that will install mods, ServerModSetup and ServerModCollectionSetup. Put the calls to the functions in this file and they will be executed on boot.
+
+--ServerModSetup takes a string of a specific mod's Workshop id. It will download and install the mod to your mod directory on boot.
+	--The Workshop id can be found at the end of the url to the mod's Workshop page.
+	--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=350811795
+	--ServerModSetup("350811795")
+
+--ServerModCollectionSetup takes a string of a specific mod's Workshop id. It will download all the mods in the collection and install them to the mod directory on boot.
+	--The Workshop id can be found at the end of the url to the collection's Workshop page.
+	--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=379114180
+	--ServerModCollectionSetup("379114180")
+
+--#Global Position
+ServerModSetup("378160973")
+
+--#Ancient Altar
+ServerModSetup("481765222")
+
+--#Health Info
+ServerModSetup("375859599")
+
+--#Superior Wendy
+ServerModSetup("1263779104")
+
+--#Pickle It
+ServerModSetup("404983266")
+
+--#Craft Stuff
+ServerModSetup("398115039")
+
+--#Deluxe Cooking Pot
+ServerModSetup("907007729")
+
+--#Recipe for Steel Wool
+ServerModSetup("473891973")
+
+--#Large Chest
+ServerModSetup("396026892")
+
+--#Fire Resurrection
+ServerModSetup("676297854")
+
+--#Reliable Webber
+ServerModSetup("1282814181")
+
+--#Mineable Gems
+ServerModSetup("380423963")
+
+--#DST Freezer
+ServerModSetup("346962876")
+
+--#Deluxe Campfires
+ServerModSetup("444235588")
+
+--#Dynamic Path Lights
+ServerModSetup("874470071")
+
+--#Miniature Dwellings
+ServerModSetup("710150780")
+
+--#Phonograph
+ServerModSetup("1727345834")
+
+--#DST Spider Cage
+ServerModSetup("384396097")
+
+--#Moving Box
+ServerModSetup("1079538195")
+
+--#New Walls - Reeds, Hedge, Mud, Living and Bone
+ServerModSetup("806289856")
+
+--#DST Storm Cellar
+ServerModSetup("382177939")
+
+--#DST Fish Farm
+ServerModSetup("356435289")
+
+--#Turfed!
+ServerModSetup("514078314")
+
+--#Kingdom - Farmer's Hut
+ServerModSetup("1768821433")
+
+--#Increased Stack Size
+ServerModSetup("374550642")
+
+--#Infinite Touch Stone
+ServerModSetup("1260611112")
+
+--#More Inventory
+ServerModSetup("378962221")
+
+--#Show Me
+ServerModSetup("666155465")
+
+--#Flower Box DST
+ServerModSetup("512502326")
