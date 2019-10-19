763de3616539b543f218635cf085d93f3648df92
GrandMasterTux <grandmaster@tuxlabs.us>
2019-10-19 09:23:31 -0400

    Update update.sh
    When the DST Server updates this file get's replaced. Copying the Master back into the directory that way during restarts of the server(game/physical) it will pull the latest mods.

--- update.sh
+++ update.sh
@@ -4,6 +4,7 @@ screen -dr dst_server2 -X quit
 cd /home/dst
 ./steamcmd.sh +login anonymous +force_install_dir /home/dst/server_dst +app_update 343050 validate +quit
 sleep 60
+cp ~/dedicated_server_mods_setup.lua /home/DST/server_dst/mods
 sh /home/dst/server_dst/bin/restart.sh
 sh /home/dst/server_dst/bin/restart2.sh
 echo "Server Started"
